422b0d998ee59b68bc7c8100c888421316870ed7
Nice Gnome <nicegnome1@gmail.com>
2023-01-13 15:48:05 -0800

    problem 1

--- problem-1.go
+++ problem-1.go
@@ -6,6 +6,20 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 package main
 
+func fizzbang(limit int) int {
+	sum := 0
+	for i := 1; i < limit; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
 func main() {
-	println("solution")
+	// test
+	println(fizzbang(10) == 23)
+
+	// solution
+	println(fizzbang(1000))
 }
